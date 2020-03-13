58bf08424b694b7e5c9377e9d54bfa1f2001e45c
Ravi <kkiran.ror@gmail.com>
2020-03-13 23:23:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,11 +34,11 @@ function viewCart() {
     default:
       for (let i = 0; i < cart.length; i++){
          if (i === 0){
-           msg = `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}`
+           msg = `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}`;
          } else if ( i === cart.length ){
-           msg += `, and ${cart[0].itemName} at $${cart[0].itemPrice}.`
+           msg += `, and ${cart[0].itemName} at $${cart[0].itemPrice}.`;
          } else {
-           msg += `, ${cart[0].itemName} at $${cart[0].itemPrice}`
+           msg += `, ${cart[0].itemName} at $${cart[0].itemPrice}`;
          }
       }
       break;
