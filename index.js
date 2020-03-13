c3d67b7495faa94ecb35773c2c2b9c2a547b4700
Ravi <kkiran.ror@gmail.com>
2020-03-13 22:53:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,6 +23,11 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
+  if (cart.length == 0){
+    return "Your shopping cart is empty.";
+  } else {
+    return `In your cart, you have ${cart[0].itemName} at ${cart[0].itemPrice}.`
+  }
 }
 
 function total() {
